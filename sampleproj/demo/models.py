2391000b308ab511d46e10015ab366d739c2aa8d
SHRIKANT RATHOD <rathodsd1710@gmail.com>
2020-08-29 14:04:32 +0530

    first commit

---
+++ sampleproj/demo/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Student(models.Model):
+    fname = models.CharField('stud_firstname',max_length=100)
+    lname = models.CharField('stud_lastname', max_length=100)
+    age = models.IntegerField("stud_age")
+    fees = models.FloatField("stud_fees")
+    address = models.CharField('stud_address', max_length=100)
+    gender = models.CharField('stud_gender', max_length=100)
+
+    class Meta: # meta class holds-- metadata about-- student structure--metadata--additional info--> data about data
+        db_table = "Stud_Info"
+
+
+
+
